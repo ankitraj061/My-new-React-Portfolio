b96b3f7e9e6880799d48c759d82cbf1f8b53ab27
Ankit Raj <ankitraj07032002@gmail.com>
2024-11-19 16:07:17 +0530

    made few changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ const App = () => {
     <main className='bg-slate-300/20'>
       <Router>
         <Navbar />
-        //hello
         <Routes>
           <Route path='/' element={<Home />} />
           <Route
